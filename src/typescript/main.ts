1c85d50ed6642814776081ffe68d98cade3624e7
Filipengbergs <filip792@hotmail.com>
2021-12-22 16:25:14 +0100

    Added getUserInput()

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -2,4 +2,18 @@ import { Todolist } from "./models/todolist";
 
 let todoList: Todolist[] = [];
 
-window.onload = function () {};
+window.onload = function () {
+    let sumbitButton: HTMLButtonElement = document.querySelector("#btn");
+    sumbitButton.addEventListener("click", getUserInput);
+};
+
+function getUserInput() {
+    let input: HTMLInputElement = document.getElementById(
+        "user-input"
+    ) as HTMLInputElement;
+
+    let newTodo: Todolist = new Todolist(input.value, false);
+    todoList.push(newTodo);
+    console.log(input.value);
+    input.value = "";
+}
